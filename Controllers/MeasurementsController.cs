7350d714d8695dcf63d6396699d51b094979a585
Simo Sirén <45048404+SimoSiren@users.noreply.github.com>
2020-07-17 02:28:43 +0300

    essential code to show data, charts still missing

---
+++ Controllers/MeasurementsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TempCharts.Models;
+
+namespace TempCharts.Controllers
+{
+    public class MeasurementsController : Controller
+    {
+        private hoivadbEntities db = new hoivadbEntities();
+
+        // GET: Measurements
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Measurements.ToListAsync());
+        }
+
+        // GET: Measurements/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Measurements measurements = await db.Measurements.FindAsync(id);
+            if (measurements == null)
+            {
+                return HttpNotFound();
+            }
+            return View(measurements);
+        }
+
+        // GET: Measurements/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Measurements/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "MeasurementID,Sender,Time,Temperature,Humidity,Pressure")] Measurements measurements)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Measurements.Add(measurements);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(measurements);
+        }
+
+        // GET: Measurements/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Measurements measurements = await db.Measurements.FindAsync(id);
+            if (measurements == null)
+            {
+                return HttpNotFound();
+            }
+            return View(measurements);
+        }
+
+        // POST: Measurements/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "MeasurementID,Sender,Time,Temperature,Humidity,Pressure")] Measurements measurements)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(measurements).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(measurements);
+        }
+
+        // GET: Measurements/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Measurements measurements = await db.Measurements.FindAsync(id);
+            if (measurements == null)
+            {
+                return HttpNotFound();
+            }
+            return View(measurements);
+        }
+
+        // POST: Measurements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Measurements measurements = await db.Measurements.FindAsync(id);
+            db.Measurements.Remove(measurements);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
